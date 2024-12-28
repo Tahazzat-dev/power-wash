80a978ed54de368b886f2076e8ec9a1fb5d12da6
Tahazzat-dev <mdtahazzatali820@gmail.com>
2024-12-28 14:09:34 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+(function($){
+  
+
+    // service card details toggler
+    $('.pw_service-details-container').on('click touch', function(){
+   var detailsContainer = $(this).find('.details');
+    // console.log(detailsContainer)
+    })
+})(jQuery)
